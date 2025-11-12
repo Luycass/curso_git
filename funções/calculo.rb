6ea51c7bfcd166280f9ac894fd7e1dc527b619ad
Lucas Batista Costa <lucasbcosta02@gmail.com>
2025-11-12 14:34:53 -0300

    atualizando a main

---
+++ funções/calculo.rb
@@ -0,0 +1,20 @@
+def soma x, y
+    x + y
+end
+
+def subt x, y
+    x - y
+end
+
+def mult x, y
+    x * y
+end
+
+def div x, y
+    x / y
+end
+
+puts soma 5,5
+puts subt 5,5
+puts mult 5,5
+puts div  5,5
